bb0de5c3a2cbb0d68ba0a59dd6f1b0515d3b059b
Roberta Pereira Marinho <robertapmarinho@gmail.com>
2024-08-20 15:39:59 -0300

    adicionado exercício 3 - JS básico

---
+++ exercicio-03/index.js
@@ -0,0 +1,13 @@
+const cadastroHospedagem = confirm('Seja bem vindo(a), aventureiro(a)! Gostaria de pagar R$ 50,00 para passar a noite na nossa hospegagem?')
+
+
+if(cadastroHospedagem) {
+  alert('Ótimo! Nós temos as melhores camas de toda a região!')
+} else{
+  alert('Que pena! Você parecia ser uma pessoa mais legal...')
+}
+
+
+/*
+cadastroHospedagem ? alert('Ótimo! Nós temos as melhores camas de toda a região!') : alert('Que pena! Você parecia ser uma pessoa mais legal...')
+*/
