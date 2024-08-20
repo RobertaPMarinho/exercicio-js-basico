f70b5d65bb5c0afd9e305a1acffa1564f0881445
Roberta Pereira Marinho <robertapmarinho@gmail.com>
2024-08-20 17:11:46 -0300

    adicionado exercício 5

---
+++ exercicio-05/index.js
@@ -0,0 +1,14 @@
+for (let x = 0; x <= 10; x++) {
+  
+  let y = 2 * x
+
+  console.log('O dobro do número ' + x + ' é ' + y)
+}
+
+/*for (let x = 0; x <= 10; x++) {
+  
+  let y = 2 * x
+
+  console.log('O dobro do número ' + x + ' é ' + x * 2)
+}
+*/
